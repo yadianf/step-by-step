99e77130242f5c16ddf5cabea0d8f8c1ca4c471d
Yadian <yadian@blueoceantech.us>
2018-03-17 18:04:03 -0400

    install certbot

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update -y
 sudo apt-get install docker-ce -y
 sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo usermod -a -G docker $USER
 
 # Install Docker-Compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
